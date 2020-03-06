f9f4bbee2b5480fd336e3e21c0161c4cfa35fcfd
epexa@bk.ru <epexa@bk.ru>
2020-03-06 17:30:07 +0800

    update project

---
+++ cordova.js
@@ -0,0 +1,11 @@
+/* uncomment for emulate ios */
+
+/* const cordova = {
+	platformId: 'ios',
+};
+
+window.appMetrica = {
+	activate: () => {}
+};
+
+window.onload = () => document.dispatchEvent(new CustomEvent('deviceready')); */
